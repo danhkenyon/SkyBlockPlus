6c2915cab64a31797c450cdc49b5756297307bd5
Laykon <nlaykon@gmail.com>
2025-11-08 01:24:49 +0000

    Added MiniMessage values to SBColour.java

--- src/main/java/uk/ac/bsfc/sbp/utils/SBColour.java
+++ src/main/java/uk/ac/bsfc/sbp/utils/SBColour.java
@@ -1,36 +1,38 @@
 package uk.ac.bsfc.sbp.utils;
 
 public enum SBColour {
-    BLACK('0', "\u001B[30m"),
-    DARK_BLUE('1', "\u001B[34m"),
-    GREEN('2', "\u001B[32m"),
-    AQUA('3', "\u001B[36m"),
-    RED('4', "\u001B[31m"),
-    MAGENTA('5', "\u001B[35m"),
-    GOLD('6', "\u001B[33m"),
-    GRAY('7', "\u001B[37m"),
-    DARK_GRAY('8', "\u001B[90m"),
-    BLUE('9', "\u001B[94m"),
-    LIME('a', "\u001B[92m"),
-    CYAN('b', "\u001B[96m"),
-    LIGHT_RED('c', "\u001B[91m"),
-    PINK('d', "\u001B[95m"),
-    YELLOW('e', "\u001B[93m"),
-    WHITE('f', "\u001B[97m"),
+    BLACK('0', "\u001B[30m", "<#000000>"),
+    DARK_BLUE('1', "\u001B[34m", "<#0000AA>"),
+    DARK_GREEN('2', "\u001B[32m", "<#00AA00>"),
+    DARK_AQUA('3', "\u001B[36m", "<#00AAAA>"),
+    DARK_RED('4', "\u001B[31m", "<#AA0000>"),
+    DARK_PURPLE('5', "\u001B[35m", "<#AA00AA>"),
+    GOLD('6', "\u001B[33m", "<#FFAA00>"),
+    GRAY('7', "\u001B[37m", "<#AAAAAA>"),
+    DARK_GRAY('8', "\u001B[90m", "<#555555>"),
+    BLUE('9', "\u001B[94m", "<#5555FF>"),
+    GREEN('a', "\u001B[92m", "<#55FF55>"),
+    AQUA('b', "\u001B[96m", "<#55FFFF>"),
+    RED('c', "\u001B[91m", "<#FF5555>"),
+    LIGHT_PURPLE('d', "\u001B[95m", "<#FF55FF>"),
+    YELLOW('e', "\u001B[93m", "<#FFFF55>"),
+    WHITE('f', "\u001B[97m", "<#FFFFFF>"),
 
-    OBFUSCATED('k', ""),
-    BOLD('l', ""),
-    STRIKETHROUGH('m', ""),
-    UNDERLINE('n', ""),
-    ITALIC('o', ""),
-    RESET('r', "\u001B[0m");
+    OBFUSCATED('k', "", "<obf>"),
+    BOLD('l', "", "<b>"),
+    STRIKETHROUGH('m', "", "<st>"),
+    UNDERLINE('n', "", "<u>"),
+    ITALIC('o', "", "<i>"),
+    RESET('r', "\u001B[0m", "<reset>");
 
     private final char mc;
     private final String ansi;
+    private final String minimessage;
 
-    SBColour(char mcCode, String ansiCode) {
+    SBColour(char mcCode, String ansiCode, String minimessage) {
         this.mc = mcCode;
         this.ansi = ansiCode;
+        this.minimessage = minimessage;
     }
 
     public char asMinecraft() {
@@ -38,5 +40,8 @@ public enum SBColour {
     }
     public String asANSI() {
         return this.ansi;
+    }
+    public String asMiniMessage(){
+        return this.minimessage;
     }
 }
