c418e6c95396fdf7d507f4147751142a986e308f
Laykon <nlaykon@gmail.com>
2025-11-16 15:26:00 +0000

    dev

---
+++ src/main/java/uk/ac/bsfc/sbp/utils/config/ServerConfig.java
@@ -0,0 +1,26 @@
+package uk.ac.bsfc.sbp.utils.config;
+
+@ConfigFile("server-config")
+public class ServerConfig implements ReloadableConfig{
+    public boolean bukkitLogging = false;
+    public boolean logTimestamp = true;
+    public String logTimestampFormat = "hh:mm:ss";
+
+    @Comment("Database connection setup")
+    public Database database = new Database();
+
+
+
+
+    public static class Database {
+        @Comment("Driver name, you can mostly ignore this")
+        public String driver = "com.mysql.jdbc.Driver";
+        @Comment("Database connection URL")
+        public String url = "jdbc:mariadb://localhost:3306/sbp";
+        @Comment("Username and password for database connection")
+        public String username = "root";
+        public String password = "admin";
+        @Comment("Maximum number of connections allowed in the pool, again can mostly ignore this")
+        public int maxPoolSize = 20;
+    }
+}
